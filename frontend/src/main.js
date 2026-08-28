1915909608c8fae1fba8f9cc8c7b7a515fc88129
zjb961221 <zjb961221@gmail.com>
2026-08-29 02:50:18 +0800

    feat: route login and registration entry pages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Login from './Login.vue'
+import Register from './Register.vue'
 import './style.css'
 
-createApp(App).mount('#app')
+const path = window.location.pathname
+const token = localStorage.getItem('support_token')
+
+if (path === '/register') {
+  createApp(Register).mount('#app')
+} else if (!token) {
+  createApp(Login).mount('#app')
+} else {
+  createApp(App).mount('#app')
+}
