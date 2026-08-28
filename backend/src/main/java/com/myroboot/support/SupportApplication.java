3c1521b28914fecb627fe8ebfb2b5cb944e1690b
zjb961221 <zjb961221@gmail.com>
2026-08-29 01:08:25 +0800

    feat: add backend application entrypoint

---
+++ backend/src/main/java/com/myroboot/support/SupportApplication.java
@@ -0,0 +1,11 @@
+package com.myroboot.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SupportApplication.class, args);
+    }
+}
