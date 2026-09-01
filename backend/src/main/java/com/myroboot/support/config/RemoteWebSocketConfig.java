7ea1d1ce3b4a8c287420251a6d58241215c770cc
zjb961221 <zjb961221@gmail.com>
2026-09-01 16:17:46 +0800

    feat: wire remote desktop tunnel service

--- backend/src/main/java/com/myroboot/support/config/RemoteWebSocketConfig.java
+++ backend/src/main/java/com/myroboot/support/config/RemoteWebSocketConfig.java
@@ -2,6 +2,7 @@ package com.myroboot.support.config;
 
 import com.myroboot.support.remote.RemoteAgentWebSocketHandler;
 import com.myroboot.support.remote.RemoteBrowserTerminalWebSocketHandler;
+import com.myroboot.support.remote.RemoteDesktopService;
 import com.myroboot.support.remote.RemoteTerminalBroker;
 import com.myroboot.support.remote.RemoteTerminalService;
 import com.myroboot.support.service.RemoteAgentService;
@@ -16,16 +17,18 @@ public class RemoteWebSocketConfig implements WebSocketConfigurer {
     private final RemoteAgentService agentService;
     private final RemoteTerminalBroker broker;
     private final RemoteTerminalService terminalService;
+    private final RemoteDesktopService desktopService;
 
-    public RemoteWebSocketConfig(RemoteAgentService agentService, RemoteTerminalBroker broker, RemoteTerminalService terminalService) {
+    public RemoteWebSocketConfig(RemoteAgentService agentService, RemoteTerminalBroker broker, RemoteTerminalService terminalService, RemoteDesktopService desktopService) {
         this.agentService = agentService;
         this.broker = broker;
         this.terminalService = terminalService;
+        this.desktopService = desktopService;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new RemoteAgentWebSocketHandler(agentService, broker, terminalService), "/api/remote/ws/agent")
+        registry.addHandler(new RemoteAgentWebSocketHandler(agentService, broker, terminalService, desktopService), "/api/remote/ws/agent")
                 .setAllowedOrigins("*");
         registry.addHandler(new RemoteBrowserTerminalWebSocketHandler(broker, terminalService), "/api/remote/ws/terminal")
                 .setAllowedOrigins("*");
